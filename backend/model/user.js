37c2a0889e0a624dbd99c347b740e36112b6d9cc
Recondito <santanallanes@hotmail.com>
2023-03-21 20:46:48 -0400

    Created User model.

---
+++ backend/model/user.js
@@ -0,0 +1,53 @@
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      set: (value) => value.replace(/\s+/g, " "),
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      set: (value) => value.replace(/\s+/g, ""),
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid.");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [6, "Password must be longer than 6 characters."],
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error('Password cannot contain the word "password".');
+        }
+      },
+    },
+    userType: {
+      type: String,
+      default: "User", //"Admin"
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    avatar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
