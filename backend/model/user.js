a225dd6b1fc91daf798abd29db7b04fb5b9f6bd2
Recondito <santanallanes@hotmail.com>
2023-03-21 20:47:56 -0400

    Forgot exporting module and requiring npm modules on User model.

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema(
   {
     username: {
