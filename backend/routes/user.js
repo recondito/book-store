b1072dac5041dd4d5df3e1249fbbcdebc0a7421a
Recondito <santanallanes@hotmail.com>
2023-04-02 20:16:13 -0400

    Added PATCH User Method.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -98,4 +98,26 @@ router.get("/user/:id/avatar", async (req, res) => {
   }
 });
 
+// PATCH User by ID.
+router.patch("/user/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Updates." });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 module.exports = router;
